df1a88d84756e07d103f6a1f391682055d0ad0a5
Andrei Itiligator <a.e.itiligator@gmail.com>
2023-09-14 11:09:07 +0700

    doc: add Doxyfile, add Ellipsoid concept docs

--- include/ellipsoid.h
+++ include/ellipsoid.h
@@ -31,13 +31,16 @@ using WSG84 = Ellipsoid<6378137, 298257223563>;
 /// \brief Реализует эллипсоид Красовского
 using PZ90 = Ellipsoid<6378136, 298257840000>;
 
+/// @private
 template<class T>
 struct is_ellipsoid_helper : std::false_type {
 };
+/// @private
 template<uint32_t a, uint64_t f_inv>
 struct is_ellipsoid_helper<Ellipsoid<a, f_inv>> : std::true_type {
 };
-
+/// \brief Концепт для проверки является ли переданный тип корректным эллипсоидом
+/// \tparam T Проверяемый тип
 template<class T>
 concept IsEllipsoid = is_ellipsoid_helper<T>::value && (T::a > 6378132) && (T::a<
         6378142) && (T::f > 1e9 / 298259000000) && (T::f < 1e9 / 298256000000);
